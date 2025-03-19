dd9e4bd0fac52195ca31446ed909d0a0472fb32a
Thiago3011 <thiago.silva1001@outlook.com>
2025-03-19 20:48:26 -0300

    adding logout and user creation routes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///instance/database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 login_manager = LoginManager()
 db.init_app(app)
@@ -33,6 +33,26 @@ def login():
 
     
     return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
+
+@app.route('/user', methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso!"})
+    
+    return jsonify({"message": "Dados inválidos"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
