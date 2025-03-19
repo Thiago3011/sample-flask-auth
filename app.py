838cc4a8a5ca3c6d45590a57e8ca9b012ac27081
Thiago3011 <thiago.silva1001@outlook.com>
2025-03-19 20:09:42 -0300

    adding de database to the repository to resolving a error with the rocketseat help

--- app.py
+++ app.py
@@ -34,9 +34,5 @@ def login():
     
     return jsonify({"message": "Credenciais inválidas"}), 400
 
-@app.route("/hello-world", methods=["GET"])
-def hello_world():
-    return "Hello World"
-
 if __name__ == "__main__":
     app.run(debug=True)
